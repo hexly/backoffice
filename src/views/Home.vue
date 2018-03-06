0246cbc01bc1e550473ef5a43214588008e2125e
Andre Kradolfer <narfdre@gmail.com>
2018-03-05 22:59:14 -0700

    beginning with dashboard cards

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,51 +1,48 @@
 <template>
   <div class="home">
-    <v-layout row>
-      <v-flex xs4 pa-3>
-        <v-card color="blue-grey darken-2" class="white--text">
-          <v-card-title primary-title>
-            <div class="headline">Unlimited music now</div>
-            <div>Listen to your favorite artists and albums whenever and wherever, online and offline.</div>
-          </v-card-title>
-          <v-card-actions>
-            <v-btn flat dark>Listen now</v-btn>
-          </v-card-actions>
-        </v-card>
-      </v-flex>
-      <v-flex xs4 pa-3>
-        <v-card color="blue-grey darken-2" class="white--text">
-          <v-card-title primary-title>
-            <div class="headline">Unlimited music now</div>
-            <div>Listen to your favorite artists and albums whenever and wherever, online and offline.</div>
-          </v-card-title>
-          <v-card-actions>
-            <v-btn flat dark>Listen now</v-btn>
-          </v-card-actions>
-        </v-card>
-      </v-flex>
-      <v-flex xs4 pa-3>
-        <v-card color="blue-grey darken-2" class="white--text">
-          <v-card-title primary-title>
-            <div class="headline">Unlimited music now</div>
-            <div>Listen to your favorite artists and albums whenever and wherever, online and offline.</div>
-          </v-card-title>
-          <v-card-actions>
-            <v-btn flat dark>Listen now</v-btn>
-          </v-card-actions>
-        </v-card>
-      </v-flex>
-    </v-layout>
+    <v-subheader>Sales</v-subheader>
+    <v-container fluid grid-list-xs>
+      <v-layout row wrap>
+        <v-flex sm4 pa-3>
+          <DashCard color="blue-grey" darken="1" display="$100" subheading="Personal" icon="person" />
+        </v-flex>
+        <v-flex sm4 pa-3>
+          <DashCard color="cyan" darken="1" display="$450" subheading="Team" icon="people" />
+        </v-flex>
+        <v-flex sm4 pa-3>
+          <DashCard color="purple" darken="1" display="4" subheading="Orders" icon="star" />
+        </v-flex>
+      </v-layout>
+    </v-container>
+    <v-subheader>Team</v-subheader>
+    <v-container fluid grid-list-xs>
+      <v-layout row wrap>
+        <v-flex sm4 pa-3>
+          <DashCard color="blue-grey" darken="2" display="23" subheading="Size" icon="person_outline" />
+        </v-flex>
+        <v-flex sm4 pa-3>
+          <DashCard color="cyan" darken="2" display="8" subheading="First Level" icon="people_outline" />
+        </v-flex>
+        <v-flex sm4 pa-3>
+          <DashCard color="purple" darken="2" display="3" subheading="Joined this Month" icon="perm_contact_calendar" />
+        </v-flex>
+      </v-layout>
+    </v-container>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import DashCard from '@/components/DashboardCard.vue'
 
 export default {
   name: 'home',
   components: {
-    HelloWorld
+    DashCard
   }
 }
 </script>
+
+<style scoped>
+
+</style>
