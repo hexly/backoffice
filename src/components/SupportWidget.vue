7a845aca02740703c260c3dfe43700745ee71e1f
Andre Kradolfer <narfdre@gmail.com>
2018-10-27 10:42:34 -0600

    adding subject

--- src/components/SupportWidget.vue
+++ src/components/SupportWidget.vue
@@ -8,7 +8,7 @@
         <v-icon large color="white darken-2">chat</v-icon>
       </v-btn>
     </div>
-    <v-alert  
+    <v-alert
       style="background-color: white;"
       :value="alert"
       color="white"
@@ -16,11 +16,12 @@
     >
       <div v-if="error">{{error}}</div>
       <div v-if="success">{{success}}</div>
-      <v-form v-if="!success">
+      <v-form v-if="!success" lazy-validation="">
         <p style="color: black;"><strong>Questions? Chat with us!</strong></p>
-        <v-text-field label="Name" v-model="name"></v-text-field>
-        <v-text-field label="Email Address" v-model="email"></v-text-field>
-        <v-textarea auto-grow label="Suport Question" v-model="message"></v-textarea>
+        <v-text-field :rules="requiredRule" label="Name" v-model="name"></v-text-field>
+        <v-text-field :rules="requiredRule" label="Subject" v-model="subject"></v-text-field>
+        <v-text-field :rules="emailRule" label="Email Address" v-model="email"></v-text-field>
+        <v-textarea :rules="requiredRule" auto-grow label="Suport Question" v-model="message"></v-textarea>
         <v-btn @click="submit" style="background-color: blue; color: white;"> Submit </v-btn>
       </v-form>
     </v-alert>
@@ -29,6 +30,7 @@
 
 <script>
 import AddSupportTicket from '@/graphql/AddSupportTicket.gql'
+const { VUE_APP_TENANT_ID } = process.env
 
 export default {
   name: 'SupportWidget',
@@ -37,9 +39,17 @@ export default {
       alert: false,
       name: this.$store.state.user.principal.member.name || '',
       email: this.$store.state.user.principal.username || '',
+      subject: '',
       message: '',
       success: false,
-      error: false
+      error: false,
+      requiredRule: [v => !!v || 'This field is required'],
+      emailRule: [
+        v => !!v || 'E-mail is required',
+        v =>
+          /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(v) ||
+          'E-mail must be valid'
+      ]
     }
   },
   methods: {
@@ -49,12 +59,28 @@ export default {
         await this.$apollo.mutate({
           mutation: AddSupportTicket,
           variables: {
-            tc: { name: this.name, email: this.email, message: this.message }
+            tc: {
+              name: this.name,
+              email: this.email,
+              message: this.message,
+              subject: this.subject,
+              tenantId: VUE_APP_TENANT_ID
+            }
           },
-          update(store, { data: ticketCreate }) {
-            this.success = `Your support ticket has been created. Your ticket number is ${
-              ticketCreate.short
-            }. For future reference please use this number.`
+          update(
+            store,
+            {
+              data: { ticketCreate },
+              error: { errors }
+            }
+          ) {
+            if (errors) {
+              this.error = errors[0]
+            } else {
+              this.success = `Your support ticket has been created. Your ticket number is ${
+                ticketCreate.short
+              }. For future reference please use this number.`
+            }
           }
         })
       } catch (err) {
