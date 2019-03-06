a16a9f729c0797c35fa20824a2c744df63f3c38f
Joshua Young <joshua.r.young1@gmail.com>
2019-03-06 13:34:28 -0700

    impersonation stuff

---
+++ src/views/Impersonate.vue
@@ -0,0 +1,54 @@
+<template>
+  <v-content>
+    <v-container
+      fluid
+      fill-height
+    >
+      <v-layout
+        align-center
+        justify-center
+      >
+        <v-flex
+          xs12
+          sm8
+        >
+          <v-card class="elevation-12">
+            <v-toolbar
+              dark
+              color="black"
+            >
+              <v-toolbar-title>Impersonate</v-toolbar-title>
+            </v-toolbar>
+            <v-card-text>
+              <router-view />
+            </v-card-text>
+          </v-card>
+        </v-flex>
+      </v-layout>
+    </v-container>
+  </v-content>
+</template>
+
+<script>
+export default {
+  name: 'ImpersonateFrame'
+}
+</script>
+
+<style scoped>
+.box-card {
+  width: 480px;
+  margin: auto;
+}
+.error {
+  margin: 5px;
+  padding: 10px;
+  text-align: center;
+}
+.logo {
+  width: 100%;
+  max-width: 250px;
+  margin: auto;
+  display: block;
+}
+</style>
