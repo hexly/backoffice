f6504a3b8dc83c0f13486adb1eb809f74060cdee
Jared Stevens <jared.b.stevens@gmail.com>
2019-08-05 16:30:28 -0500

    team card completed

--- src/components/TeamCard.vue
+++ src/components/TeamCard.vue
@@ -1,43 +1,55 @@
 <template>
   <v-card
-    max-width="500"
-    class="ma-2 pa-2"
+  max-width="350"
+  class="ma-2 pa-2"
   >
-    <div v-if="loading">
-      <v-progress-circular indeterminate :size="50" :width="5" color="primary"></v-progress-circular>
-    </div>
 
     <v-img
       :src="getAvatar"
+      contain
       class="cardImg white--text"
       gradient="to bottom, rgba(0,0,0,.1), rgba(0,0,0,.5)"
     >
     <v-card-title class="fill-height align-end">
-      <h2>{{(user.name).toUpperCase()}}</h2>
-      <h4>{{(user.contacts[0].emails[0].email).toLowerCase()}}</h4>
+      <v-flex row>
+        <h2>{{(user.name).toUpperCase()}}</h2>
+        <h4 v-if="hasEmail">{{(user.contacts[0].emails[0].email).toLowerCase()}}</h4>
+      </v-flex>
     </v-card-title>
     </v-img>
 
-    <v-card-text v-if="!loading">
-      <span v-if="stats && stats.joinedOn"><br>
-        <span>Joined {{formatDate(stats.joinedOn)}}</span><br>
-        <span>Tribe Size: {{stats.teamSize || 0}}</span><br>
-        <span>Front Line: {{stats.firstLevelSize || 0}}</span><br>
-        <span>Total Points: {{stats.totalPoints ? stats.totalPoints.toFixed(2) : 0}}</span>
-        </span>
-      <span v-else>{{noData}}</span>
-    </v-card-text>
-    <div width=100%>
-      <v-card-actions v-if="actions" class="justify-space-between">
-        <v-btn flat color="secondary" @click="viewTeam">View Team</v-btn>
-        <v-btn flat color="secondary" v-if="isQualified">Qualified</v-btn>
-        <v-btn flat color=white disabled v-else>Unqualified</v-btn>
-      </v-card-actions>
-    </div>
+      <v-flex v-if="loading" d-flex justify-center align-center class="text-xs-center">
+        <v-progress-circular indeterminate :size="50" :width="5" color="primary"></v-progress-circular>
+      </v-flex>
+
+      <v-flex row>
+        <v-card-text v-if="!loading">
+          <div v-if="stats && stats.joinedOn">
+            <div>
+            <span>Joined: {{$moment(stats.joinedOn).format('ll')}}</span>
+            <span> -- </span>
+            <span>{{formatDate(stats.joinedOn)}}</span>
+            </div>
+            <div>Tribe Size: {{stats.teamSize || 0}}</div>
+            <div>Front Line: {{stats.firstLevelSize || 0}}</div>
+            <div>Total Points: {{stats.totalPoints ? stats.totalPoints.toFixed(2) : 0}}</div>
+          </div>
+          <div v-else>{{noData}}</div>
+        </v-card-text>
+      </v-flex>
+
+    <v-divider v-if="actions" class="primary"/>
+    <v-card-actions v-if="actions" class="justify-space-between">
+      <v-btn flat color="secondary" @click="viewTeam">View Team</v-btn>
+      <v-btn flat color="primary" v-if="isQualified">Qualified</v-btn>
+      <v-btn flat color=white disabled v-else>Unqualified</v-btn>
+    </v-card-actions>
+
   </v-card>
 </template>
 
 <script>
+
 export default {
   name: 'TeamCard',
   data() {
@@ -88,9 +100,9 @@ export default {
 </script>
 
 <style scoped>
-.cardImg {
+/* .cardImg {
   margin: auto;
-  max-height: 50%;
+  max-height: 100%;
   max-width: 100%;
-}
+} */
 </style>
