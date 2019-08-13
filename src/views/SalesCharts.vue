4457b0fc0701739c7738c9629886ca1a63d89396
Jared Stevens <jaredstevens@Merlin.local>
2019-08-13 16:48:27 -0500

    figuring stuff out

--- src/views/SalesCharts.vue
+++ src/views/SalesCharts.vue
@@ -5,18 +5,20 @@
     </v-toolbar>
     <div>
       <GChart
-      class="pa-2 ma-2"
-      type="ColumnChart"
-      :data="chartData"
-      :options="chartOptions"
+        class="pa-2 ma-2"
+        type="ColumnChart"
+        :data="chartData"
+        :options="chartOptions"
       />
     </div>
     <div>
       <GChart
-      class="pa-2 ma-2"
-      type="ColumnChart"
-      :data="chartData"
-      :options="chartOptions"
+        class="pa-2 ma-2"
+        :settings="{packages: ['bar']}"
+        :data="chartData"
+        :options="chartOptions"
+        :createChart="(el, google) => new google.charts.Bar(el)"
+        @ready="onChartReady"
       />
     </div>
     <div>
@@ -62,16 +64,34 @@ export default {
   components: { GChart },
   data () {
     return {
+      chartsLib: null,
       chartData: [
         ['Year', 'Sales', 'Expenses', 'Profit'],
         ['2014', 1000, 400, 200],
         ['2015', 1170, 460, 250],
         ['2016', 660, 1120, 300],
         ['2017', 1030, 540, 350]
-      ],
-      chartOptions: {
-        title: 'Company Performance'
-      }
+      ]
+    }
+  },
+  computed: {
+    chartOptions () {
+      if (!this.chartsLib) return null
+      return this.chartsLib.charts.Bar.convertOptions({
+        chart: {
+          title: 'Company Performance',
+          subtitle: 'Sales, Expenses, and Profit: 2014-2017'
+        },
+        bars: 'horizontal',
+        hAxis: { format: 'decimal' },
+        height: 800,
+        colors: ['#1b9e77', '#d95f02', '#7570b3']
+      })
+    }
+  },
+  methods: {
+    onChartReady (chart, google) {
+      this.chartsLib = google
     }
   }
 }
